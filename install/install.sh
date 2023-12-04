9b1f2e6df8a3c2eee7b3eede74e69401f1331f5c
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 06:45:31 +0000

    move glibc

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 systemctl stop reflector.service
 mount -o remount,size=75% /run/archiso/cowspace
-pacman -Sy --noconfirm --needed reflector
+pacman -Sy --noconfirm --needed glibc reflector
 reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -x berkeley
-pacman -Syu glibc --noconfirm --ignore linux
+pacman -Syu --noconfirm --ignore linux
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_configuration.json -O
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_credentials.json -O
 archinstall --config user_configuration.json --creds user_credentials.json
