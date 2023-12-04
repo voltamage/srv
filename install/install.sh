1a08ba0bfc611f2f9247fb765270d77dc0d8c324
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 19:28:54 +0000

    print instead of run because of EOF bug

--- install/install.sh
+++ install/install.sh
@@ -5,4 +5,4 @@ reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -
 pacman -Syu --noconfirm --ignore linux
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_configuration.json -O
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_credentials.json -O
-archinstall --config user_configuration.json --creds user_credentials.json
+print archinstall --config user_configuration.json --creds user_credentials.json
