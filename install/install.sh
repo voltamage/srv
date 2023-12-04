43b9b09faca83fab1e398aab426cfb433df1ab73
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 05:01:25 +0000

    forgot noconfirm

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ mount -o remount,size=75% /run/archiso/cowspace
 pacman-key --init
 pacman -Sy --needed --noconfirm reflector
 reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -x berkeley
-pacman -Syu --ignore linux
+pacman -Syu --noconfirm --ignore linux
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_configuration.json -O
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_credentials.json -O
 archinstall --config user_configuration.json --creds user_credentials.json
