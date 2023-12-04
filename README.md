c91035c55196c17fc75780f935a81a7b0d5b0a86
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 11:54:45 -0800

    update installer config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/install.sh | sh
+archinstall --config user_configuration.json --creds user_credentials.json
--- install/install.sh
+++ install/install.sh
@@ -5,4 +5,3 @@ reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -
 pacman -Syu --noconfirm --ignore linux
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_configuration.json -O
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_credentials.json -O
-print archinstall --config user_configuration.json --creds user_credentials.json
