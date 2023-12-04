f2ca6887aa8306abd08ca2a4d25426370d8fd7fa
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 05:17:06 +0000

    pacman key populate

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@
 systemctl stop reflector.service
 mount -o remount,size=75% /run/archiso/cowspace
 pacman-key --init
+pacman-key --populate
 pacman -Sy --needed --noconfirm reflector
 reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -x berkeley
 pacman -Syu --noconfirm --ignore linux
