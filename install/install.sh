cf76f47e9b50fa2b3b59f29e301fb725932dc693
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-04 05:30:12 +0000

    archlinux-keyring

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 systemctl stop reflector.service
 mount -o remount,size=75% /run/archiso/cowspace
+pacman -Sy --noconfirm archlinux-keyring
 pacman-key --init
 pacman-key --populate
-pacman -Sy --needed --noconfirm reflector
+pacman -S --noconfirm --needed reflector
 reflector --sort rate -c US -p https --delay 1 --save /etc/pacman.d/mirrorlist -x berkeley
 pacman -Syu --noconfirm --ignore linux
 curl https://raw.githubusercontent.com/voltamage/srv/main/install/user_configuration.json -O
