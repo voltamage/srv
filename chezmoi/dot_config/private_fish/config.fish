59e10d01749034a1b46540e7eb4185ed4ef9dab9
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-05 15:20:21 -0800

    aliases

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -18,11 +18,18 @@ if ! builtin functions --query __zoxide_cd_internal
 	end
 end
 
+alias c='chezmoi'
 alias cd='z'
 alias cdi='zi'
+alias cdif='chezmoi diff'
+alias cgit='lazygit -p ~/srv'
 alias dd='cd ~/srv && sudo docker-compose down'
 alias du='cd ~/srv && sudo docker-compose up -d'
+alias e='nvim'
 alias ff='fd --color=always -H | fzf --ansi'
+alias ls='eza -ao'
 alias mt='sh ~/srv/maintain.sh'
+alias q='exit'
+alias sf='source ~/.config/fish/config.fish'
 
 bind \e\[B 'complete'
