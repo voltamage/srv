46ab84aa6a435082ce0df95acd360e1e567d7008
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-05 10:25:30 -0800

    fix sudoers

--- install/setup.sh
+++ install/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo pacman -Sy --noconfirm --needed ansible git
 git clone https://github.com/voltamage/srv
-sudo ansible-playbook ~/srv/install/vault.yml --ask-vault-pass
+sudo ansible-playbook ~/srv/install/init.yml --ask-vault-pass
 sudo ansible-playbook ~/srv/system.yml
 chezmoi apply
 sudo ansible-playbook ~/srv/cockpit.yml
