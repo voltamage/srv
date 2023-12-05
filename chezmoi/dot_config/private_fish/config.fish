7a98706ba683620e2c17e33d022a80ceb6115908
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-05 14:20:09 -0800

    added ansible alias

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 atuin init fish | source
 zoxide init fish | source
 
+function ap
+	sudo ansible-playbook ~/srv/$argv.yml
+end
+
 if ! builtin functions --query __zoxide_cd_internal
 	if builtin functions --query cd
 		builtin functions --copy cd __zoxide_cd_internal
