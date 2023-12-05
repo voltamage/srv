d9b8dff8b5a662d8e6ada5b134299b41e5cd70a7
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-05 14:34:28 -0800

    added fuzzy finding and config editing

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -5,6 +5,10 @@ function ap
 	sudo ansible-playbook ~/srv/$argv.yml
 end
 
+function ep
+	nvim ~/srv/$argv.yml
+end
+
 if ! builtin functions --query __zoxide_cd_internal
 	if builtin functions --query cd
 		builtin functions --copy cd __zoxide_cd_internal
@@ -17,6 +21,7 @@ alias cd='z'
 alias cdi='zi'
 alias dd='cd ~/srv && sudo docker-compose down'
 alias du='cd ~/srv && sudo docker-compose up -d'
+alias ff='fd --color=always -H | fzf --ansi'
 alias mt='sh ~/srv/maintain.sh'
 
 bind \e\[B 'complete'
