f77714e1ff7adac21fe3e7c0de585ad0c942f59c
voltamage <37029882+voltamage@users.noreply.github.com>
2023-12-05 20:04:48 -0800

    removed fish greeting

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -1,23 +1,3 @@
-atuin init fish | source
-starship init fish | source
-zoxide init fish | source
-
-function ap
-	sudo ansible-playbook ~/srv/$argv.yml
-end
-
-function ep
-	nvim ~/srv/$argv.yml
-end
-
-if ! builtin functions --query __zoxide_cd_internal
-	if builtin functions --query cd
-		builtin functions --copy cd __zoxide_cd_internal
-	else
-		alias __zoxide_cd_internal='builtin cd'
-	end
-end
-
 alias c='chezmoi'
 alias cd='z'
 alias cdi='zi'
